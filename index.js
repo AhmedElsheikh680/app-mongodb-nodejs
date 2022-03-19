e026a22301cdb180a15caaece8cad8139d81a0b7
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-03-19 14:25:36 -0700

    update Employee

--- index.js
+++ index.js
@@ -32,10 +32,25 @@ const Employee = mongoose.model('Employee', employeeSchema);
 
 async function getEmployees(){
    const emps =  await Employee.
-   find().
+   // find({name: 'Ali'}).
+   // find({age: {$gte: 10}}).
+   //     find({age: {$in: [25,40]}}).
+   find({age: {$nin: [25,40]}}).
    sort({name: 1})
-       .limit(2)
+       .limit(20)
    .select({name: 1, age:1,department:1, isApproved:1})
    console.log(emps);
 }
-getEmployees();
+//getEmployees();
+
+async function updateEmployee(id) {
+     const emp =await Employee.findById(id);
+     if(!emp) {
+         console.log('Employee not Found!!');
+     }
+     emp.name="Ahmed Updated";
+     const data = await emp.save();
+     console.log(data);
+
+}
+updateEmployee('62363b06049cdc37ec77359d');
