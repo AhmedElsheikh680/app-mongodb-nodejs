271e6adfccc67e7eafd848a4c1fb2d618c98cfd6
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-03-19 13:24:29 -0700

    create employee

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb://localhost/employees',{
     useNewurlParser: true,
 })
 .then(()=>console.log("Connected Successfully!!"))
 .catch((e)=>console.error("Connected Failed!!"+e))
 
-console.log("Ahmed Elsheikh");
+const employeeSchema = new mongoose.Schema({
+
+    name: String,
+    age: {type: Number, min: 24, max: 60},
+    department: [String],
+    date: {type:Date, default: Date.now()},
+    isApproved: Boolean
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+ async function createEmployee() {
+    const emp = new Employee({
+        name: "Sayed",
+        age: 54,
+        department: ["C# Developer", "Nodejs Developer"],
+        isApproved: true
+    });
+   const data =  await emp.save();
+   console.log(data);
+}
+createEmployee();
